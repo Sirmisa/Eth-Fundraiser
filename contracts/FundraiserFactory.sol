ff6d223c5858be33a4ce10939d789e86dcb59fe0
Misa <you@example.com>
2021-07-05 21:44:40 -0500

    adding the function to handle the fundraisers and the tests for the limit size of results that we will retrieve when we try to fetch the fundraisers

--- contracts/FundraiserFactory.sol
+++ contracts/FundraiserFactory.sol
@@ -8,6 +8,9 @@ contract FundraiserFactory {
 
    event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
 
+   // most items that can be returned from fundraisers function
+    uint256 constant maxLimit = 20;
+
    function fundraisersCount() public view returns(uint256) {
        return _fundraisers.length;
    }
@@ -32,5 +35,28 @@ function createFundraiser(
     );// when we initialize the contracts we receive back the address of the new instance msg.sender
     _fundraisers.push(fundraiser);
     emit FundraiserCreated(fundraiser, msg.sender);
+}
+
+function fundraisers(uint256 limit, uint256 offset) public view 
+    returns(Fundraiser[] memory coll) {
+    // this method triggers an index out of bounds exception, the EVM will throw an opcode
+    // to handle this before that error occurs we can add a validation to avoid that EVM error
+    // but consider that even doing that it is possible that inside the function we still chack values that dont exist
+    require(offset <= fundraisersCount(), "offset out of bounds");
+
+    // size should be the smaller of the count or the limit
+    uint256 size = fundraisersCount() - offset;
+    size = size < limit ? size : limit;
+    // size should not exceed the maxLimit
+    size = size < maxLimit ? size : maxLimit;
+    coll = new Fundraiser[](size);
+    
+    for(uint256 i = 0; i < size; i++) {
+        // here we are assigning values from the actual fundraisers, instead of 0x000... accounts
+        // In the case where the offset is larger than our fundraisersCount, 
+        // our application will need to exit with an out-of-bounds message.
+        coll[i] = _fundraisers[offset + i];
+    }
+    return coll;
 }
 }
